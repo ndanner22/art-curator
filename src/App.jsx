3d7948752ace438db39fcd68b2790dd27e7dd008
Neil Danner <ndanner22@gmail.com>
2024-09-16 15:43:40 +0100

    create HomePage component and set it to display a specific image from the Met API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import HomePage from "./Components/HomePage";
 
 function App() {
   return (
     <>
-      <h1>Art Curator</h1>
+      <HomePage />
     </>
   );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 export const getArtById = (artId) => {
-  const baseURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`;
+  const baseUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`;
   return axios
     .get(baseUrl)
     .then(({ data }) => {
-      console.log(data);
       return { data };
     })
     .catch(({ response }) => {
