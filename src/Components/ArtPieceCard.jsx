05b3586731ff6e13ad8ad74fddf65c9880956c65
Neil Danner <ndanner22@gmail.com>
2024-09-22 13:29:58 +0100

    change to ArtPieceCard layout

--- src/Components/ArtPieceCard.jsx
+++ src/Components/ArtPieceCard.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-const ArtworkItem = ({ artwork, addToCollection }) => {
+const ArtworkItem = ({ artwork }) => {
   return (
     <div>
       <img src={artwork.image} alt={artwork.title} />
       <h3>{artwork.title}</h3>
       <p>{artwork.artist}</p>
-      <button onClick={() => addToCollection(artwork)}>
-        Add to Collection
-      </button>
     </div>
   );
 };
