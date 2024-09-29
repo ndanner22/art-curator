dd2ca61e8105bf69f835021b4f25ac2ae4dfc182
Neil Danner <ndanner22@gmail.com>
2024-09-29 18:52:20 +0100

    Fix App.css so that the display card in the user's collection card will stay a fixed size and the image will remain a fixed height.

--- src/Components/CollectionArtPieceCard.jsx
+++ src/Components/CollectionArtPieceCard.jsx
@@ -2,18 +2,22 @@ const CollectionArtPieceCard = ({ artWork }) => {
   return (
     <div className="collection">
       {artWork.image ? (
-        <img src={artWork.image} alt={artWork.title} width="200px" />
+        <div className="image-container">
+          <img src={artWork.image} alt={artWork.title} width="200px" />
+        </div>
       ) : (
         <div className="no-image">No Image Available 😔</div>
       )}
-      <h3>{artWork.title}</h3>
-      <p>Artist: {artWork.artist}</p>
-      <p>Museum: {artWork.api}</p>
-      <p>Date: {artWork.date}</p>
-      <p>Location of piece: {artWork.api}</p>
-      <a href={artWork.info} target="_blank" rel="noopener noreferrer">
-        More Information
-      </a>
+      <div className="collection-info">
+        <h3>{artWork.title}</h3>
+        <p>Artist: {artWork.artist}</p>
+        <p>Museum: {artWork.api}</p>
+        <p>Date: {artWork.date}</p>
+        <p>Location of piece: {artWork.api}</p>
+        <a href={artWork.info} target="_blank" rel="noopener noreferrer">
+          More Information
+        </a>
+      </div>
     </div>
   );
 };
